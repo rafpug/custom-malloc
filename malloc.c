5c69724dde2dc888c48899a4c7b02bb2af6545b1
rafpug <salcedop@calpoly.edu>
2026-01-09 14:50:07 -0800

    Feat: add calloc debug

--- malloc.c
+++ malloc.c
@@ -75,22 +75,49 @@ int add_block(){
 
 
 Header *scan_headers_size(size_t target_size){
-	Header *cur_header = head
-	
+	Header *cur_header = head;
 	while(cur_header){
 		if (cur_header->block_size 
-		&& cur_header->block_size > target_size){
+		&& cur_header->block_size > target_size
+		&& cur_header->free){
 			return cur_header;
 		} 
 		else if (cur_header->next){
-			cur_header = next;
+			cur_header = cur_header->next;
 		}
 		else {
-			return NULL;
+			break;
 		}
 	}
+	return NULL;
 		
 }
+
+Header *scan_headers_address(void *init_ptr){
+	Header *cur_header = head;
+
+	while(cur_header){
+		if (cur_header <= init_ptr
+		&& init_ptr < cur_header + cur_header->block_size 
+		+ alignment_up(sizeof(Header))){
+			return cur_header;
+		}
+		else if (cur_header->next){
+			cur_header = cur_header->next;
+		}
+		else{
+			break;
+		}
+	}
+	return NULL;	
+}
+
+Header *split_block(Header *init_block, size_t size){
+	if (init_block && init_block->block_size
+	> alignment_up(size) + alignment_up(sizeof(Header)){
+		
+	}
+	
 
 void *malloc(size_t size){
 	if (init_head) {
@@ -116,13 +143,16 @@ void *malloc(size_t size){
 	new_header->block_size = cur_header->block_size - target_size;
 	cur_header->block_size = alignment_up(size);
 		
-	new_header->free = 1;
+	new_header->free = 0;
 	cur_header->free = 0;
+	
+	free(new_header);
 	
 	void *finalptr = cur_header + alignment_up(sizeof(Header));
 		
 	if(getenv("DEBUG_MALLOC")){
-                pp(stderr, "MALLOC: malloc(%d)    => (ptr=%p, size=%d)\n", size, finalptr, cur_header->block_size);
+                pp(stderr, "MALLOC: malloc(%d)    => (ptr=%p, size=%d)\n", 
+			size, finalptr, cur_header->block_size);
         }
 
 	return finalptr;		
@@ -132,6 +162,14 @@ void *calloc(size_t count, size_t size){
 	void *finalptr = malloc(count*size);
 	if (finalptr){ 
 		memset(finalptr, 0, size*count);
+
+		if (getenv("DEBGUG_MALLOC")){
+			Header *final_header = scan_headers_address(finalptr);
+			pp(stderr, "MALLOC: calloc(%d,%d)   
+				=> (ptr=%p, size=%d)", count, size, finalptr, 
+				final_header->block_size 
+				+ alignment_up(sizeof(header)));
+		}
 		return finalptr;
 	}
 	return NULL;	
@@ -142,9 +180,26 @@ void *realloc(void *ptr, size_t size){
 		return malloc(size);
 	} 
 	else if (ptr && size == 0) {
-		free(ptr)
-		return NULL
+		free(ptr);
+		return NULL;
 	}
+	
+	Header *original_header = scan_headers_address(ptr);
+	if(original_header){
+		if (original_header->block_size > size 
+		+ alignment_up(sizeof(Header))){
+			
+		} 
+			
+		if(original_header->next && original_header->next->free){
+			size_t merged_size = original_header->block_size
+				+ original_header->next->block_size;
+			
+			if(merged_size >= alignment_up(size)){
+				
+			}
+			
+		
 	
 	
 }
