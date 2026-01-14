9d8e6d677ff392345205112fdbbe32926baff63b
rafpug <salcedop@calpoly.edu>
2026-01-14 12:39:53 -0800

    Fix: styling inconsistencies

--- malloc.c
+++ malloc.c
@@ -23,14 +23,14 @@ typedef struct Header {
 	struct Header *prev;
 } Header; 
 
-Header *head = NULL;
-Header *tail = NULL;
+static Header *head = NULL;
+static Header *tail = NULL;
 
 /* alignment_up: Accepts a size and returns the 
  * 	size at the next 16 byte step 	
  */
-size_t alignment_up(size_t req_size){
-        if (req_size % ALIGNMENT == 0){
+static size_t alignment_up(size_t req_size) {
+        if (req_size % ALIGNMENT == 0) {
                 return req_size;
         }
         else {
@@ -46,7 +46,7 @@ size_t alignment_up(size_t req_size){
  * Returns zero on success
  * Returns non-zero on failure
  */
-int add_block(){
+static int add_block() {
 	void *old_break = sbrk(BLOCK_SIZE);
         if (old_break == (void *) -1) {
 		// Failed to get memory from the OS
@@ -55,14 +55,14 @@ int add_block(){
 
 	// Ensures that the pointer for our new block is 16 byte aligned
 	uintptr_t old_int = (uintptr_t) old_break;
-        if (old_int % ALIGNMENT != 0){
+        if (old_int % ALIGNMENT != 0) {
                	old_int += ALIGNMENT - (old_int % ALIGNMENT);
         }
 
 	
  	// Adds the memory based to our linked list based on the scenario
-	if(tail){
-		if (tail->free){
+	if(tail) {
+		if (tail->free) {
 			// Case: Our tail is free
 			// The tail block is expanded
 			tail->block_size += BLOCK_SIZE;
@@ -100,24 +100,24 @@ int add_block(){
 }
 
 /* scan_headers_size: Scans through our linked list 
- * 	in search of free nodes that can fit specified size
+ * 	in search of free block that can fit specified size
  *
- * Returns NULL when no node can fit the size
+ * Returns NULL when no block can fit the size
  *
  * scan_headers_size doesn't guarantee that 
- * 	the node can be split unless the inputted
+ * 	the block can be split unless the inputted
  * 	size already accounts for the header_size
  */
-Header *scan_headers_size(size_t target_size){
+static Header *scan_headers_size(size_t target_size) {
 	Header *cur_header = head;
-	while(cur_header){
+	while(cur_header) {
 		
 		if (cur_header->block_size 
 		&& cur_header->block_size >= target_size
-		&& cur_header->free){
+		&& cur_header->free) {
 			return cur_header;
 		} 
-		else if (cur_header->next){
+		else if (cur_header->next) {
 			cur_header = cur_header->next;
 		}
 		else {
@@ -139,20 +139,20 @@ Header *scan_headers_size(size_t target_size){
  * Mainly used to pinpoint a pointer for free() and realloc()
  * I also used it to find the previous block in the list
  */
-Header *scan_headers_address(void *init_ptr){
+static Header *scan_headers_address(void *init_ptr) {
 	Header *cur_header = head;
 
-	while(cur_header){
+	while(cur_header) {
 		if ((void *) cur_header <= init_ptr
 		&& (uintptr_t) init_ptr < (uintptr_t) cur_header 
 		+ cur_header->block_size 
-		+ HEADER_SIZE){
+		+ HEADER_SIZE) {
 			return cur_header;
 		}
-		else if (cur_header->next){
+		else if (cur_header->next) {
 			cur_header = cur_header->next;
 		}
-		else{
+		else {
 			break;
 		}
 	}
@@ -166,14 +166,14 @@ Header *scan_headers_address(void *init_ptr){
  * Returns NULL when there is no next block 
  * 	or when the next block isn't free 
  */
-Header *merge_next_block(Header *header1){
+static Header *merge_next_block(Header *header1) {
 	Header *header2 = header1->next;
-	if(header1 && header2 && header2->free){
+	if(header1 && header2 && header2->free) {
 		header1->next = header2->next;
 		header1->block_size += header2->block_size 
 			+ HEADER_SIZE;
 		Header *header3 = header2->next;
-		if(header3){
+		if(header3) {
 			header3->prev = header1;
 		}
 		else {
@@ -194,9 +194,11 @@ Header *merge_next_block(Header *header1){
  * Returns NULL when the given block isn't big enough to be split
  *
  */
-Header *split_block(Header *cur_header, size_t size){
+static Header *split_block(Header *cur_header, size_t size) {
+
         size_t target_size = HEADER_SIZE + alignment_up(size);
-        if (cur_header && cur_header->block_size >= target_size){
+
+        if (cur_header && cur_header->block_size >= target_size) {
 
                 uintptr_t new_ptr = (uintptr_t) cur_header + target_size;
                 Header *new_header = (Header *)new_ptr;
@@ -230,9 +232,9 @@ Header *split_block(Header *cur_header, size_t size){
  * Returns a pointer to the allocated space upon success
  * Returns NULL upon failure
  */
-void *malloc(size_t size){
+void *malloc(size_t size) {
 	
-	if (size <= 0){
+	if (!size) {
 		return NULL;
 	}
 	
@@ -242,10 +244,10 @@ void *malloc(size_t size){
 		+ HEADER_SIZE;
 	
 	Header *cur_header = scan_headers_size(target_size);
-	while (!cur_header){
+	while (!cur_header) {
 		// Repeatedly adds blocks until one reaches
 		// 	the target_size
-		if(add_block()){
+		if(add_block()) {
 			// If add_block() returns non-zero
 			// then we failed to get more memory
 			// from the OS
@@ -267,7 +269,7 @@ void *malloc(size_t size){
 	uintptr_t finalintptr = (uintptr_t) cur_header + HEADER_SIZE;
 	void *finalptr = (void *) finalintptr;
 		
-	if(getenv("DEBUG_MALLOC")){
+	if(getenv("DEBUG_MALLOC")) {
 		char buf[256];
                 snprintf(buf, sizeof(buf), 
 			"MALLOC: malloc(%zu)    => (ptr=%p, size=%zu)\n", 
@@ -285,19 +287,19 @@ void *malloc(size_t size){
  * Returns a pointer to the memory upon success
  * Returns NULL upon failure
  */
-void *calloc(size_t count, size_t size){
+void *calloc(size_t count, size_t size) {
 
 	// Implicitly calls malloc
 	void *finalptr = malloc(count*size);
 
-	if (finalptr){ 
+	if (finalptr) { 
 
 		// Upon a successful malloc call
 		// 	memset is used to init
 		// 	memory to zero
 		memset(finalptr, 0, size*count);
 
-		if (getenv("DEBUG_MALLOC")){
+		if (getenv("DEBUG_MALLOC")) {
 			Header *final_header = scan_headers_address(finalptr);
 
 			char buf[256];
@@ -319,7 +321,7 @@ void *calloc(size_t count, size_t size){
  * 	Returns a pointer to the resized/relocated block upon success
  * 	Returns NULL upon failure
  */
-void *realloc(void *ptr, size_t size){
+void *realloc(void *ptr, size_t size) {
 	
 	// Special cases of realloc()
 	if (!ptr) {
@@ -335,39 +337,39 @@ void *realloc(void *ptr, size_t size){
 	void *old_ptr = ptr; // Stored for debugging 
 	
 	// Goes through various checks to determine what to do with the block
-	if(original_header){
+	if(original_header) {
 		// Case: block containing ptr was found
 		uintptr_t payload_intptr = (uintptr_t) original_header 
 						+ HEADER_SIZE;
 		
 		if(!split_block(original_header,size) 
-		&& original_header->block_size < alignment_up(size)){
+		&& original_header->block_size < alignment_up(size)) {
 			// Case: splitting failed because desired size
 			// 	exceeded the block_size so the
 			// 	block needs to be expanded
-			if(original_header->next){
+			if(original_header->next) {
 				// Case: block has a next block so 
 				// 	it must not be the tail
 				
 				while(original_header->block_size
-				< alignment_up(size)){
+				< alignment_up(size)) {
 					// Repeatedly merges the next block
 					// 	until it encounters one that
 					// 	is already allocated
 
-					if(!merge_next_block(original_header)){
+					if(!merge_next_block(original_header)) {
 						break;
 					}
 				}
 				
 				if (original_header->block_size
-				< alignment_up(size)){
+				< alignment_up(size)) {
 					// Case: not enough space to resize
 
 					// Malloc+memcpy instead of expanding
 					void *finalptr = malloc(size);
 					
-					if (!finalptr){
+					if (!finalptr) {
 						return NULL;
 					}
 					memcpy(finalptr
@@ -384,7 +386,7 @@ void *realloc(void *ptr, size_t size){
 					ptr = (void *) payload_intptr; 
 				} 
 			}
-			else{
+			else {
 				// Case: block has no next, so it must be the
 				// 	tail of our linked list
 
@@ -396,8 +398,8 @@ void *realloc(void *ptr, size_t size){
  				 if its free 		 
  				*/ 				
 				while(original_header->block_size 
-				< alignment_up(size)){
-					if(add_block()){
+				< alignment_up(size)) {
+					if(add_block()) {
 						errno = ENOMEM;
 						return NULL;
 					}
@@ -427,7 +429,7 @@ void *realloc(void *ptr, size_t size){
 		return NULL;
 	}
 
-	if (getenv("DEBUG_MALLOC")){
+	if (getenv("DEBUG_MALLOC")) {
         	Header *final_header = scan_headers_address(ptr);
 
 		char buf[256];
@@ -443,8 +445,8 @@ void *realloc(void *ptr, size_t size){
 /* free: Marks the block associated with the given pointer as free
  * 	Additionally merges the block with its next and previous block
  */
-void free(void *ptr){
-	if (!ptr){
+void free(void *ptr) {
+	if (!ptr) {
 		return;
 	}
 	
@@ -460,7 +462,7 @@ void free(void *ptr){
 
 		// Finds the previous block and merges it if its free
 		Header *prev_header = target_header->prev;
-		if (prev_header && prev_header->free){
+		if (prev_header && prev_header->free) {
 			target_header = merge_next_block(prev_header);
 		}
 
@@ -470,16 +472,16 @@ void free(void *ptr){
 		// 	our 64k block size. This prevents tanking performance
 		// 	when repeatedly allocating and freeing
 		if (!target_header->next
-			&& target_header->block_size > BLOCK_SIZE){
+			&& target_header->block_size > BLOCK_SIZE) {
 			// Case: We are freeing the tail of our list
 			
 			// Marks the previous block, if any, as the tail
 			prev_header = target_header->prev;
-			if (prev_header){
+			if (prev_header) {
 				tail = prev_header;
 				prev_header->next = NULL;
 			}
-			else if (target_header == head){
+			else if (target_header == head) {
 				// Updates the head appropriately
 				head = NULL;
 				tail = NULL;
@@ -491,7 +493,7 @@ void free(void *ptr){
 		}
 	}
 
-	if(getenv("DEBUG_MALLOC")){
+	if(getenv("DEBUG_MALLOC")) {
 		char buf[256];
 		snprintf(buf, sizeof(buf), "MALLOC: free(%p)\n", ptr);
 		fputs(buf, stderr);
